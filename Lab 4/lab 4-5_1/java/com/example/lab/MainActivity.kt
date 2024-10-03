c68fef9138d3ccfd9afaa7f5e4c641605847651d
NguyenMinhTriet-2002 <nguyenminhtriet2002@gmail.com>
2024-10-03 22:33:08 +0700

    Update MainActivity.kt

--- Lab 4/lab 4-5_1/java/com/example/lab/MainActivity.kt
+++ Lab 4/lab 4-5_1/java/com/example/lab/MainActivity.kt
@@ -19,7 +19,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.composearticle.ui.theme.ComposeArticleTheme
+import com.example.lab.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
