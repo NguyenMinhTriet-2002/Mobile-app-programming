e0d64a76248234775782bc394fbd33e02a88c25c
NguyenMinhTriet-2002 <nguyenminhtriet2002@gmail.com>
2024-11-03 23:14:15 +0700

    Add files via upload

---
+++ Lab 8/Lab 8_3-4/androidTest/java/com/lab/reply/test/TestAnnotations.kt
@@ -0,0 +1,5 @@
+package com.lab.reply.test
+
+annotation class TestCompactWidth
+annotation class TestMediumWidth
+annotation class TestExpandedWidth
---
+++ Lab 8/Lab 8_3-4/main/java/com/lab/reply/ui/ReplyUiState.kt
@@ -0,0 +1,14 @@
+package com.lab.reply.ui
+
+import com.lab.reply.data.Email
+import com.lab.reply.data.MailboxType
+import com.lab.reply.data.local.LocalEmailsDataProvider
+
+data class ReplyUiState(
+    val mailboxes: Map<MailboxType, List<Email>> = emptyMap(),
+    val currentMailbox: MailboxType = MailboxType.Inbox,
+    val currentSelectedEmail: Email = LocalEmailsDataProvider.defaultEmail,
+    val isShowingHomepage: Boolean = true
+) {
+    val currentMailboxEmails: List<Email> by lazy { mailboxes[currentMailbox]!! }
+}
