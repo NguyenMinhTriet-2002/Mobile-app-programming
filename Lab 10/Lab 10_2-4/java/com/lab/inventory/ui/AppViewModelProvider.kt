3b759959af6fa85b371b71ab57ff67b25a6e837a
NguyenMinhTriet-2002 <nguyenminhtriet2002@gmail.com>
2024-11-04 09:33:54 +0700

    Add files via upload

---
+++ Lab 10/Lab 10_2-4/java/com/lab/inventory/ui/AppViewModelProvider.kt
@@ -0,0 +1,50 @@
+package com.lab.inventory.ui
+
+import android.app.Application
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.lab.inventory.InventoryApplication
+import com.lab.inventory.ui.home.HomeViewModel
+import com.lab.inventory.ui.item.ItemDetailsViewModel
+import com.lab.inventory.ui.item.ItemEditViewModel
+import com.lab.inventory.ui.item.ItemEntryViewModel
+
+/**
+ * Provides Factory to create instance of ViewModel for the entire Inventory app
+ */
+object AppViewModelProvider {
+    val Factory = viewModelFactory {
+        // Initializer for ItemEditViewModel
+        initializer {
+            ItemEditViewModel(
+                this.createSavedStateHandle()
+            )
+        }
+        // Initializer for ItemEntryViewModel
+        initializer {
+            ItemEntryViewModel(inventoryApplication().container.itemsRepository)
+        }
+
+        // Initializer for ItemDetailsViewModel
+        initializer {
+            ItemDetailsViewModel(
+                this.createSavedStateHandle()
+            )
+        }
+
+        // Initializer for HomeViewModel
+        initializer {
+            HomeViewModel()
+        }
+    }
+}
+
+/**
+ * Extension function to queries for [Application] object and returns an instance of
+ * [InventoryApplication].
+ */
+fun CreationExtras.inventoryApplication(): InventoryApplication =
+    (this[AndroidViewModelFactory.APPLICATION_KEY] as InventoryApplication)
